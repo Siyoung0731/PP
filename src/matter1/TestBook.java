d009698970986403ca13cb572fb66cdbe525102e
Siyoung0731 <azxx0731@gmail.com>
2026-04-01 23:38:18 +0900

    TestBook 수정 및 완료
    Updated BookVo to use String for cdate and adjusted related methods. Modified process and output methods to handle date calculations and formatting.

--- src/matter1/TestBook.java
+++ src/matter1/TestBook.java
@@ -3,6 +3,8 @@ package matter1;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.Scanner;
 
 
@@ -18,7 +20,7 @@ class BookVo {
 	private int num;
 	private String name;
 	private String bname;
-	private int cdate;
+	private String cdate;
 	private int rcdate;
 	private char member;
 	
@@ -26,7 +28,7 @@ class BookVo {
 	private String cgrade;
 	private String mname;
 	
-	public BookVo(int num, String name, String bname, int cdate, int rcdate, char member) {
+	public BookVo(int num, String name, String bname, String cdate, int rcdate, char member) {
 		this.num = num;
 		this.name = name;
 		this.bname = bname;
@@ -58,11 +60,11 @@ class BookVo {
 		this.bname = bname;
 	}
 
-	public int getCdate() {
+	public String getCdate() {
 		return cdate;
 	}
 
-	public void setCdate(int cdate) {
+	public void setCdate(String cdate) {
 		this.cdate = cdate;
 	}
 
@@ -123,7 +125,7 @@ class BookCheck implements Bpo {
 		int num = Integer.parseInt(li[0].trim());
 		String name = li[1].trim();
 		String bname = li[2].trim();
-		int cdate = Integer.parseInt(li[3].trim());
+		String cdate = li[3].trim();
 		int rcdate = Integer.parseInt(li[4].trim());
 		char member = li[5].trim().charAt(0);
 		
@@ -133,15 +135,56 @@ class BookCheck implements Bpo {
 	@Override
 	public void process() {
 		//대출일수 = 현재날짜 기준으로 대출일로부터 지난 일수
-		int today = LocalDate.now().getDayOfYear();
-		System.out.println(today);
+		LocalDate today = LocalDate.now();
+		String cdate = bVo.getCdate();
+		
+		int cyear = Integer.parseInt(cdate.substring(0, 4));
+		int cmonth = Integer.parseInt(cdate.substring(4, 6));
+		int cday = Integer.parseInt(cdate.substring(6, 8));
+		
+		LocalDate loandate = LocalDate.of(cyear, cmonth, cmonth);
+		long daysBetween = ChronoUnit.DAYS.between(loandate, today);
+		bVo.setCnum((int) daysBetween);
 		//대출등급 = 7일 이하 정상, 8~14일 주의, 15일 이상 연체
+		String cgrade = "";
+		int cnum = bVo.getCnum();
+		if(cnum <= 7) {
+			cgrade = "정상";
+		} else {
+			if (cnum >= 8 && cnum <= 14) {
+				cgrade = "주의";
+			} else {
+				if (cnum >= 15) {
+					cgrade = "연체";
+				}
+			}
+		}
+		bVo.setCgrade(cgrade);
 		//회원구분명 = A:일반회원, B:우수회원, C:특별회원
+		char member = bVo.getMember();
+		String mname = "";
+		switch (member) {
+		case 'A': mname = "일반회원"; break;
+		case 'B': mname = "우수회원"; break;
+		case 'C': mname = "특별회원"; break;
+		default:
+			break;
+		}
+		bVo.setMname(mname);
 		//날짜 형식은 yyyyMMdd
 	}
 
 	@Override
 	public void output() {
+		String title = "회원번호 이름 도서명 대출일수 대출등급 회원구분명";
+		String fmt = "%d %s %s %d %s %s";
+		int num = bVo.getNum();
+		String name = bVo.getName();
+		String bname = bVo.getBname();
+		int cnum = bVo.getCnum();
+		String cgrade = bVo.getCgrade();
+		String mname = bVo.getMname();
+		System.out.printf(fmt, num, name, bname, cnum, cgrade, mname);
 	}
 	
 }
